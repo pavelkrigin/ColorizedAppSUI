c0faf2ed720f792356ab1437b25c0f867cbf2ca6
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-10-22 23:33:20 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Pavel Krigin on 22.10.22..
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
